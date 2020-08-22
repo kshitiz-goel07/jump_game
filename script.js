c98f2f2cfcf578e296a19b5ae03fe85c4fe5ffda
Kshitiz Goel <57189655+kshitiz-goel07@users.noreply.github.com>
2020-08-23 02:12:06 +0530

    Final commit

--- script.js
+++ script.js
@@ -8,3 +8,12 @@ function jump(){
 character.classList.remove("animate");
     },500);
 }
+var checkdead = setInterval(function(){
+    var characterTop=parseInt(window.getComputedStyle(character).getPropertyValue("top"));
+    var blockleft=parseInt(window.getComputedStyle(block).getPropertyValue("left"));
+    if(blockleft<20 && blockleft>0 && characterTop>=130){
+        block.style.animation="none";
+        block.style.display="none";
+        alert("You are definately not good in games !!\nRefresh the page to play again");
+    }
+},10)
